949cecb45ee167b987215d7b4d400c92008fc5d8
jerryang76 <jerryang76@yahoo.com.tw>
2016-12-22 17:52:46 +0800

    Update Telnet_login.py

--- Telnet_login.py
+++ Telnet_login.py
@@ -1,38 +1,46 @@
 #Password ***
 import getpass
-#import sys
+import sys
 import telnetlib
 # pause
 import os
 
-HOST = raw_input("Octtel IP address: ")
-#HOST = "10.10.1.224"
+HOST = raw_input("Telnet server IP address: ")
+#HOST = "10.10.1.223"
 
 user = raw_input("Enter your remote account: ")
-#user = "aslkdfj"
+#user = "oc"
 
 password = getpass.getpass()
-#password = "2134"
+#password = "128"
 
 times = input("Retry times: ")
-if times > 10:
-	print "Number",times,"too big, max is 10."
-	times = 10
+#times = 3
+if times > 100:
+	print "Number",times,"too big, max is 100."
+	times = 100
 
-print "Attacking :", HOST
+print "Attacking :", HOST, "port 23"
 
 while times > 0:
 	i = 3
-	tn = telnetlib.Telnet(HOST)
+	tn = telnetlib.Telnet(HOST, 23, 10)	
 	while i > 0:
-		tn.read_until("r: ")
-		tn.write(user + "\r\n")
+		tn.read_until("r: ")		
+		print "<< Command Line Interface V 3.6.3.0 >>"
+		print ""
+		tn.write(user + "\r\n")		
+		print "User: "+ user
 		if password:
 			tn.read_until("d: ")
-			tn.write(password + "\r\n")
+			tn.write(password + "\r\n")	
+			print "Password: "+password
+			print "****"
+			print "Incorrect User/Password, Please Retry."
+			print ""
 		i -= 1
 	print tn.read_all()
 	tn.close()
-	print "Tries left: ",times-1
+	print "						Tries left: ",times-1
 	times -= 1
 os.system("pause")
